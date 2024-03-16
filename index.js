f3ec68f8306d6691364b8786a6f841b46ac328e2
Muhammad-javed440 <141915304+Muhammad-javed440@users.noreply.github.com>
2024-03-17 01:16:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Please enter firstNumber",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Please enter secondNumber",
+        type: "number",
+        name: "secondNumber",
+    },
+    {
+        message: "select one of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["+", "-", "*", "/"],
+    },
+]);
+// conditional statement
+if (answer.operator === "+") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "-") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "*") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "/") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
